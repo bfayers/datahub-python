7595ce558a8751cb26acfa1e3194d2dbfb619bb2
bfayers <bfayers@users.noreply.github.com>
2021-01-15 17:49:12 +0000

    .1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="metoffice-datahub",
     packages=find_packages(include=["datahub"]),
-    version="0.5",
+    version="0.5.1",
     description="MetOffice DataHub Library",
     author="bfayers",
     license="GPLv3",
