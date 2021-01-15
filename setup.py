40ef2de69109e676dd1498bb96127cc0793c2507
bfayers <bfayers@users.noreply.github.com>
2021-01-15 16:31:50 +0000

    set version to 0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='datahub',
     packages=find_packages(include=['datahub']),
-    version='0.0.1',
+    version='0.5',
     description='MetOffice DataHub Library',
     author='bfayers',
     license='GPLv3',
